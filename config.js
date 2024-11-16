e1d10aa0986d89e7813c36df34c4d910f5f9e5e1
lilpica <lilpica22@gmail.com>
2024-11-16 19:24:03 +0200

    Update config.js

--- config.js
+++ config.js
@@ -39,9 +39,9 @@ module.exports = {
   nodes: [
     {
         name: "Server",
-        host: "nodev4.fypmoon.org",
-        port: 1118,
-        password: "fypmoon.org",
+        host: "lava.catfein.com",
+        port: 4000,
+        password: "catfein",
         reconnectTimeout: 5000,
         reconnectTries: Infinity,
         secure: false
